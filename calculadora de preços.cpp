510ec4e913ca526ee78468679059114ce2aeede7
arthurcelestinoe <arthurcelestino243@gmail.com>
2025-07-17 16:30:23 -0300

    Removidos acentos e cedilha do código

--- calculadora de preços.cpp
+++ calculadora de preços.cpp
@@ -1,8 +1,8 @@
 /*
 Autor: Arthur Celestino da Silva
 Ano: 2025
-Funcionalidade: Calcular automaticamente os preços da Gasolina e do Etanol Ipimax Aditivados a partir do preço dos seus
-respectivos comuns e calcular os preços com o acrescimo TicketLog/GoodCard.
+Funcionalidade: Calcular automaticamente os precos da Gasolina e do Etanol Ipimax Aditivados a partir do preco dos seus
+respectivos comuns e calcular os precos com o acrescimo TicketLog/GoodCard.
 */
 
 #include <iostream>
@@ -17,11 +17,11 @@ int main(){
     double etaipit = 0.0;
     double gasipit = 0.0;
 
-    std::cout << "Bem-Vindo!\nEste programa calcula os preços do GoodCard/TicketLog e dos combustíveis aditivados automaticamente." << std::endl;
-    std::cout << "IMPORTANTE! Para que este programa funcione corretamente use . para separar as csas decimais do inteiro. EX.: 4.60, 6.15 7.29" << std::endl;
-    std::cout << "Para começarmos informe o preço da Gasolina Comum Orignal: R$ " << std::endl;
+    std::cout << "Bem-Vindo!\nEste programa calcula os precos do GoodCard/TicketLog e dos combustiveis aditivados automaticamente." << std::endl;
+    std::cout << "IMPORTANTE! Para que este programa funcione corretamente use . para separar as casas decimais do inteiro. EX.: 4.60, 6.15 7.29" << std::endl;
+    std::cout << "Para comecarmos informe o preco da Gasolina Comum Original: R$ " << std::endl;
     std::cin >> gascom;
-    std::cout << "Agora infome o preço do Etanol Comum Original: R$ " << std::endl;
+    std::cout << "Agora informe o preco do Etanol Comum Original: R$ " << std::endl;
     std::cin >> etacom;
     std::cout <<"-----------------------------------------------------------" <<std::endl;
 
@@ -33,13 +33,13 @@ int main(){
     etacomt = etacom + 0.20;
     etaipit = etaipi + 0.20;
 
-    std::cout << "O Preço do Etanol Ipimax Aditivado é R$ " << etaipi << std::endl;
-    std::cout << "O preço da Gasolina Ipimax Aditivada é R$ " << gasipi <<std::endl;
+    std::cout << "O preco do Etanol Ipimax Aditivado e R$ " << etaipi << std::endl;
+    std::cout << "O preco da Gasolina Ipimax Aditivada e R$ " << gasipi <<std::endl;
     std::cout << "----------------------------------------------------------" <<std::endl;
-    std::cout << "O preço da Gasolina Original Comum no TicketLog é R$ " << gascomt <<std::endl;
-    std::cout << "O preço da Gasolina Ipimax Aditivada no TicketLog é R$ " << gasipit <<std::endl;
-    std::cout << "O Preço do Etanol Original Comum no TicketLog é R$ " << etacomt <<std::endl;
-    std::cout << "O Preço do Etanol Ipimax Aditivado no TicketLog é R$ " << etaipit <<std::endl;
+    std::cout << "O preco da Gasolina Original Comum no TicketLog e R$ " << gascomt <<std::endl;
+    std::cout << "O preco da Gasolina Ipimax Aditivada no TicketLog e R$ " << gasipit <<std::endl;
+    std::cout << "O preco do Etanol Original Comum no TicketLog e R$ " << etacomt <<std::endl;
+    std::cout << "O preco do Etanol Ipimax Aditivado no TicketLog e R$ " << etaipit <<std::endl;
 
     std::cout << "Pressione ENTER para sair...";
     std::cin.ignore();  // Limpa o \n que ficou no buffer (caso exista)
